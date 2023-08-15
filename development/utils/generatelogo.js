7ea794cd65e81c564e9967b7c95b6a719f2bf9fe
David Liebherr <ambition.aidev@gmail.com>
2023-08-14 22:14:14 -0500

    updated some codes

--- development/utils/generatelogo.js
+++ development/utils/generatelogo.js
@@ -6,21 +6,21 @@ export const render = (response) => {
 
 
     let { charc, txtColor, shape, shapeColor, x, y, r } = response
-    const usersShape = `${shape}`
+    let xml
 
-    if (usersShape === 'circle') {
-        shape = circleRenderSVGcode;
+    if (shape === 'circle') {
+        xml = circleRenderSVGcode;
     }
-     else if (usersShape === 'triangle') {
-        shape = triangleRenderSVGcode;
-    } else if (usersShape === 'square'){
-        shape = squareRenderSVGcode;
+    else if (shape === 'triangle') {
+        xml = triangleRenderSVGcode;
+    } else if (shape === 'square') {
+        xml = squareRenderSVGcode;
     }
 
     return `
     <svg width='300' height='200' xmlns="http://www.w3.org/2000/svg">
     
-    ${shape} fill="${shapeColor}"/>  
+    ${xml} fill="${shapeColor}"/>  
    
 
     <text x="150" y="125" alignment-baseline="middle" text-anchor="middle" font-size="55" fill="${txtColor}">${charc}</text>
